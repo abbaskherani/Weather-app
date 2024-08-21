44209415c85bb98f0110f5908b8bc11603c0cd7a
abbaskherani <abbaskherani1998@gmail.com>
2024-08-21 17:03:14 +0530

    New Project creation

---
+++ src/components/Weather.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import humidity_icon from "../assets/humidity.png";
+
+const Weather = () => {
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt="" />
+      </div>
+
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default Weather;
